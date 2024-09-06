e3f5537cb7d2423d40aa85274c6be092784b52b7
JDragon <lee411806@naver.com>
2024-09-06 22:32:59 +0900

    폴더 생성 (폴더 db에 있나 조회)

--- src/main/java/com/sparta/myselectshop/repository/FolderRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/FolderRepository.java
@@ -1,7 +1,13 @@
 package com.sparta.myselectshop.repository;
 
 import com.sparta.myselectshop.entity.Folder;
+import com.sparta.myselectshop.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FolderRepository extends JpaRepository<Folder, Long> {
+    List<Folder> findAllByUserAndNameIn(User user, List<String> forderNames);
+    // 쿼리메서드 쿼리 짜보기
+    // select * from folder where user_id = ? and name in (폴더이름1, 폴더이름2, 폴더이름3 여기에 폴더이름 들어옴)
 }
