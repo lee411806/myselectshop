9091b96c5cdb044cb70a6d7b9975821eabcaa934
Jdragon <lee411806@naver.com>
2024-10-30 02:10:27 +0900

    controller test

---
+++ src/test/java/com/sparta/myselectshop/mvc/MockSpringSecurityFilter.java
@@ -0,0 +1,26 @@
+package com.sparta.myselectshop.mvc;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.io.IOException;
+
+public class MockSpringSecurityFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        // 인증객체를 실제로 만드는 것 -> 가짜 인증을 하는 행위 , 가짜 필터를 만든다. (테스트하는데 방해되기 때문에)
+        SecurityContextHolder.getContext()
+                .setAuthentication((Authentication) ((HttpServletRequest) req).getUserPrincipal());
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        SecurityContextHolder.clearContext();
+    }
+}
